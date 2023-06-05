b850376e315eceae7831576482a81c39e37f515f
Minh Tran <93815818+Conabot@users.noreply.github.com>
2023-06-05 00:07:46 -0700

    complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import react from 'react';
+
+function EyesOnMe(){
+
+    function handleFocus(e){
+        console.log("Good!")
+    }
+    function handleBlur(e){
+        console.log("Hey! Eyes on me!")
+    }
+    return (
+        <div>
+            <button onFocus={handleFocus}  onBlur={handleBlur}>Eyes on me</button>
+        </div>
+    )
+}
+export default EyesOnMe;
